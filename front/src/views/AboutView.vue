57749dc7f962b48fc8c7a1f18d4401cd88daefea
Arthur <arthurbratigny@gmail.com>
2025-03-13 12:29:28 +0100

    reset front

---
+++ front/src/views/AboutView.vue
@@ -0,0 +1,29 @@
+<script>
+import Button from 'primevue/button'
+import {InputText} from "primevue";
+
+export default {
+  components: {
+    Button,
+    InputText
+  }
+}
+</script>
+<template>
+  <div class="about">
+    <div class="card flex justify-center">
+      <Button label="Verify" />
+      <InputText placeholder="Enter your email" />
+    </div>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
